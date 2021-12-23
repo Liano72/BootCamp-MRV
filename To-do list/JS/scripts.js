e1754170e75d7cd94b44e2abe1e34d56d6e64cec
Lucas Liano <89361754+Liano72@users.noreply.github.com>
2021-12-23 18:26:06 -0300

    Add files via upload

---
+++ To-do list/JS/scripts.js
@@ -0,0 +1,9 @@
+var captar = "";
+
+function chamar() {
+        e.preventDefault();
+        captar = document.getElementById("texto").value;
+        document.getElementById("digito").innerHTML = captar;
+};
+
+// Meu codigo esta com erro no tipo de entrada de dados, tirei um print
